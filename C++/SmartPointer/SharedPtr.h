e4742305c73b4ee13fc281329d0ee8d0aeefcdf1
loureed927 <ltbuaa@gmail.com>
2018-01-08 01:54:14 +0800

    Add SharedPtr.h.

--- C++/SmartPointer/SharedPtr.h
+++ C++/SmartPointer/SharedPtr.h
@@ -5,26 +5,40 @@
 #ifndef SHAREDPTR_H
 #define SHAREDPTR_H
 
+// multiple SharedPtr can share/own same pointer which avoid redundant storage
+// of same resource, destroy pointer when there is not any SharedPtr own it.
+// reference counting is used to count the owning number.
+
+// SharedPtr objects can only share ownership by copying their value.
+// if two SharedPtr are constructed from same pointer, they are NOT sharing.
+
+// Attempt 1:
+// failed: copying shared ptr will increase its reference count, but not copied shared ptr.
+/*
 template<typename T>
 class SharedPtr
 {
 public:
     SharedPtr(T* p = nullptr) :rawPointer(p)
     {
+        referenceCount = 1;
     }
 
     ~SharedPtr()
     {
-        delete rawPointer;
+        if (referenceCount==0)
+            delete rawPointer;
     }
 
-    SharedPtr(SharedPtr& sp) :rawPointer(sp.release())
+    SharedPtr(SharedPtr& sp) :rawPointer(sp.get())
     {
+        referenceCount++;
     }
 
     SharedPtr<T>& operator=(SharedPtr<T>& sp)
     {
-        reset(sp.release());
+        reset(sp.get());
+        referenceCount++;
 
         return *this;
     }
@@ -49,26 +63,98 @@ public:
         return rawPointer;
     }
 
-    // return raw pointer and give up ownership.
-    T* release()
+    // destroy raw pointer, make managing sharedptr object empty.
+    void reset()
     {
-        T* temp = rawPointer;
-        rawPointer = nullptr;
-        return temp;
-    }
-
-    // destroy raw pointer and store new one.
-    void reset(T* p = nullptr)
-    {
-        if (p != rawPointer)
+        if (rawPointer != nullptr)
         {
             delete rawPointer;
         }
-        rawPointer = p;
     }
 
 private:
     T* rawPointer;
+    int referenceCount;
 };
+*/
+
+// Attempt 2:
+// failed: copying and copied shared ptr will both increase its reference count.
+// but not for other shared ptr, e.g, SharedPtr A, B, C, B copy A, C copy B.
+// only B and C update its reference count correctly, see copy ctor.
+
+// so it is not right to put referenceCount directly on the field of SharedPtr.
+// Need to have an object which contain reference count and raw pointer, and make
+// SharedPtr own the pointer to this object. This will make all sharedPtr own the same
+// referenceCount object which leads to Attempt 3.
+/*
+template<typename T>
+class SharedPtr
+{
+public:
+    SharedPtr(T* p = nullptr) :rawPointer(p)
+    {
+        referenceCount = 1;
+    }
+
+    ~SharedPtr()
+    {
+        if (referenceCount == 0)
+            delete rawPointer;
+    }
+
+    SharedPtr(SharedPtr& sp) :rawPointer(sp.get())
+    {
+        // need to update reference count of sp and this shared ptr.
+        sp.referenceCount++;
+        referenceCount = sp.referenceCount;
+    }
+
+    SharedPtr<T>& operator=(SharedPtr<T>& sp)
+    {
+        rawPointer = sp.get();
+        sp.referenceCount++;
+        referenceCount = sp.referenceCount;
+
+        return *this;
+    }
+
+    T& operator*()
+    {
+        if (rawPointer == nullptr)
+            throw rawPointer;
+        return *rawPointer;
+    }
+
+    T* operator->()
+    {
+        if (rawPointer == nullptr)
+            throw rawPointer;
+        return rawPointer;
+    }
+
+    // return raw pointer.
+    T* get() const
+    {
+        return rawPointer;
+    }
+
+    // destroy raw pointer, make managing sharedptr object empty.
+    void reset()
+    {
+        if (rawPointer != nullptr)
+        {
+            delete rawPointer;
+        }
+    }
+
+private:
+    T* rawPointer;
+    int referenceCount;
+};
+*/
+
+// Attempt 3:
+
 
 #endif
--- C++/SmartPointer/Test.cpp
+++ C++/SmartPointer/Test.cpp
@@ -3,7 +3,30 @@
 //**************************************************************
 
 #include "AutoPtr.h"
+#include "SharedPtr.h"
 #include <memory>
+#include <iostream>
+
+using namespace std;
+// test object
+class Base
+{
+public:
+    Base(){};
+    ~Base(){};
+
+    void func()
+    {
+        cout << "Base func call." << endl;
+    };
+};
+
+class Derive :public Base
+{
+public:
+    Derive(){};
+    ~Derive(){};
+};
 
 void AutoPtr_Test()
 {
@@ -20,14 +43,37 @@ void AutoPtr_Test()
     AutoPtr<int> spccc;
     spccc = spcc;
 
+    // test for operator * and ->
+    AutoPtr<Base> b(new Base);
+    b->func();
+    (*b).func();
+
+    // multiple auto_ptr own same objects which delete raw pointer multiple times. Incorrect behavior.
     int* pp = new int(5);
     std::auto_ptr<int> ap(pp);
     std::auto_ptr<int> apc(ap);
     std::auto_ptr<int> apcc = apc;
 }
 
+void SharedPtr_Test()
+{
+    int* pp = new int(5);
+    SharedPtr<int> sp(pp);
+    SharedPtr<int> spp(sp);
+    SharedPtr<int> sppp = spp;
+    sppp.reset();
+
+    //int* pp = new int(5);
+    //std::shared_ptr<int> sp(pp);
+    //std::shared_ptr<int> spother(pp); // anther shared ptr, ref count=1.
+    //std::shared_ptr<int> spp(sp);
+    //std::shared_ptr<int> sppp = spp;
+    //sppp.reset();
+}
+
 int main()
 {
     AutoPtr_Test();
+    SharedPtr_Test();
 }
 
