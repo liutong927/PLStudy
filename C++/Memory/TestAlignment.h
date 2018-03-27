1dd3f38329856965586e25bc6948163e8d91b6e9
loureed927 <ltbuaa@gmail.com>
2018-03-28 02:01:44 +0800

    Separate TestAlignment.h from TestMemory.cpp.

---
+++ C++/Memory/TestAlignment.h
@@ -0,0 +1,35 @@
+//**************************************************************
+//         Test routine for storage alignment
+//**************************************************************
+
+#include <iostream>
+
+using namespace std;
+
+struct MyStruct
+{
+    char a;         // 1 byte, padding 3
+    int b;          // 4 bytes
+    short c;        // 2 bytes, padding 6
+    long long d;    // 8 bytes
+    char e;         // 1 byte, padding 7
+};
+
+void TestAlignment()
+{
+    cout << "Size of MyStruct is: " << sizeof(MyStruct) << endl;
+    cout << "Alignment of MyStruct is: " << alignment_of<MyStruct>::value << endl;
+
+    MyStruct ms;
+    ms.a = 'a';
+    ms.b = 1;
+    ms.c = 2;
+    ms.d = 3;
+    ms.e = 'b';
+    cout << "Address of MyStruct a: " << hex << (long)&(ms.a) << endl;
+    cout << "Address of MyStruct b: " << hex << (long)&(ms.b) << endl;
+    cout << "Address of MyStruct c: " << hex << (long)&(ms.c) << endl;
+    cout << "Address of MyStruct d: " << hex << (long)&(ms.d) << endl;
+    cout << "Address of MyStruct e: " << hex << (long)&(ms.e) << endl;
+}
+
